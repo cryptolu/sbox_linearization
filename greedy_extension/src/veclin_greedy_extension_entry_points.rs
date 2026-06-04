7ce28ec928cc60604fd0222858cafcfb2fa0f575
hellman <hellman@users.noreply.github.com>
2026-06-04 13:35:27 +0200

    make init/seed optional

--- greedy_extension/src/veclin_greedy_extension_entry_points.rs
+++ greedy_extension/src/veclin_greedy_extension_entry_points.rs
@@ -4,6 +4,9 @@ use crate::greedy_extension::greedy_extension_py;
 use crate::implemented_algorithms::GreedyExtensionU16;
 
 #[pyfunction]
-pub fn greedy_extension_u16(py: Python, mut alg: PyRefMut<AlgorithmU16>, init: Vec<u16>, seed: u64) -> PyResult<Vec<u16>>  {
+#[pyo3(signature = (alg, init=None, seed=None))]
+pub fn greedy_extension_u16(py: Python, mut alg: PyRefMut<GreedyExtensionU16>, init: Option<Vec<u16>>, seed: Option<u64>) -> PyResult<Vec<u16>> {
+    let init = init.unwrap_or_default();
+    let seed = seed.unwrap_or_else(|| rand::thread_rng().gen());
     Ok(greedy_extension_py(py, &mut alg.implemented, &init, seed)?)
 }
